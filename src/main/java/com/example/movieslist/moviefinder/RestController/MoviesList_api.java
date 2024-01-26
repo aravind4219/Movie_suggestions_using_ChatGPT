f1e3473881722ea3fa0f9f889f715646d8dd36c8
Aravind Kumar Dhodda <aravind4219@gmail.com>
2024-01-25 23:28:50 -0500

    added html template.

--- src/main/java/com/example/movieslist/moviefinder/RestController/MoviesList_api.java
+++ src/main/java/com/example/movieslist/moviefinder/RestController/MoviesList_api.java
@@ -1,12 +1,19 @@
 package com.example.movieslist.moviefinder.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class MoviesList_api {
    private ConnecttoChatGpt askGpt;
 
-    @RequestMapping ("/")
+    @RequestMapping("/")
+    public String SearchBox()
+    {
+        return "searchbox";
+    }
+    @RequestMapping ("chatgpt")
     public String MoviesList()
 
     {
