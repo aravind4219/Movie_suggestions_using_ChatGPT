3d334cde2f887d5018ff0b82424f317d5623a635
Aravind Kumar Dhodda <aravind4219@gmail.com>
2024-02-05 23:19:45 -0500

    Duplicate commit

--- src/main/java/com/example/movieslist/moviefinder/RestController/MoviesList_api.java
+++ src/main/java/com/example/movieslist/moviefinder/RestController/MoviesList_api.java
@@ -17,13 +17,11 @@ public class MoviesList_api {
     }
     @RequestMapping ("chatgpt")
     public String MoviesList()
-
     {
         String prompt="give me the List of 10 telugu movies released in 2010";
         String model="gpt-3.5-turbo";
         String requestbody = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
         askGpt=new ConnecttoChatGpt("apikey","https://api.openai.com/v1/chat/completions",requestbody);
-
         return "response";
     }
 }
